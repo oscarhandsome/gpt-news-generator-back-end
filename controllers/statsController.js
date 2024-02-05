f77f2fcaa3f8fad6228dca008923bd14e8f19df3
Askhat Bagaviyev <altair5555@gmail.com>
2024-02-05 15:09:05 +0600

    added raw stats

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,8 +1,14 @@
 const catchAsync = require('../utils/catchAsync');
+// const News = require('./../models/newsModel');
+// const Booking = require('./../models/bookingModel');
+// const User = require('./../models/userModel');
 // const AppError = require('../utils/appError');
 
 exports.getInfo = catchAsync(async (req, res, next) => {
-  console.log(req);
+  // const news = await News.find();
+  // const bookings = await Booking.find();
+  // const users = await User.find();
+
   //   if (!news) {
   //     return next(new AppError('No news found with that ID', 404));
   //   }
