1b9f4104dd7fa0aa3f7da8c38295f7fd5e6abf18
oscarhandsome <altair5555@gmail.com>
2024-03-12 02:29:13 +0500

    added filter

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -11,7 +11,10 @@ class APIFeatures {
 
     // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lte|lt|eq)\b/g,
+      match => `$${match}`
+    );
 
     this.query.find(JSON.parse(queryStr));
 
